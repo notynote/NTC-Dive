4f2b7ab5450a0117923a466d113892f0404853ed
Admin <admin@Admins-MacBook-Pro.local>
2018-11-26 19:28:51 +0700

    Changed schedule

--- src/client/components/organisms/Schedule/Containers.js
+++ src/client/components/organisms/Schedule/Containers.js
@@ -4,67 +4,7 @@ import ScheduleContainer  from './ScheduleContainer'
 
 
 
-export const Container1 = ({ translate }) => (
 
-        <Fragment>
-           <ScheduleContainer month={translate("Schedule.oct")} >
-            <TripsContainer
-            tripCode='PRC1-2018'
-            dates='06-07 Oct 2018'
-            boat='January Princess'
-            avalibility='2'
-            />
-            <TripsContainer
-            tripCode='PRC2-2018'
-            dates='13-14 Oct 2018'
-            boat='January Princess'
-            avalibility='Full'
-            />
-            <TripsContainer 
-            tripCode='PRC3-2018'
-            dates='20-21 Oct 2018'
-            boat='January Princess'
-            avalibility='6'
-            />
-            <TripsContainer 
-            tripCode='PRC4-2018'
-            dates='27-28 Oct 2018'
-            boat='January Princess'
-            avalibility='Full'
-            />
-          </ScheduleContainer>
-        </Fragment>
-)
-export const Container2 = ({ translate }) => (
-        <Fragment>
-           <ScheduleContainer month={translate("Schedule.nov")} >
-            <TripsContainer
-            tripCode='PRC5-2018'
-            dates='03-04 Nov 2018'
-            boat='January Princess'
-            avalibility='Full'
-            />
-            <TripsContainer
-            tripCode='PRC6-2018'
-            dates='10-11 Nov 2018'
-            boat='January Princess'
-            avalibility='Full'
-            />
-            <TripsContainer 
-            tripCode='PRC7-2018'
-            dates='17-18 Nov 2018'
-            boat='January Princess'
-            avalibility='Full'
-            />
-            <TripsContainer 
-            tripCode='PRC8-2018'
-            dates='24-25 Nov 2018'
-            boat='January Princess'
-            avalibility='Full'
-            />
-          </ScheduleContainer>
-        </Fragment>
-)
 export const Container3 = ({ translate }) => (
         <Fragment>
            <ScheduleContainer month={translate("Schedule.dec")} >
@@ -72,7 +12,7 @@ export const Container3 = ({ translate }) => (
             tripCode='PRC9-2018'
             dates='01-02 Dec 2018'
             boat='January Princess'
-            avalibility='16'
+            avalibility='Full'
             />
             <TripsContainer
             tripCode='PRC10-2018'
@@ -84,7 +24,7 @@ export const Container3 = ({ translate }) => (
             tripCode='PRC11-2018'
             dates='15-16 Dec 2018'
             boat='January Princess'
-            avalibility='2'
+            avalibility='Full'
             />
             <TripsContainer 
             tripCode='PRC12-2018'
@@ -96,7 +36,7 @@ export const Container3 = ({ translate }) => (
             tripCode='PRC13-2018'
             dates='29-30 Dec 2018'
             boat='January Princess'
-            avalibility='12'
+            avalibility='7 Slots'
             />
           </ScheduleContainer>
         </Fragment>
@@ -108,7 +48,7 @@ export const Container4 = ({ translate }) => (
             tripCode='PRC14-2019'
             dates='05-06 Jan 2019'
             boat='January Princess'
-            avalibility='19'
+            avalibility='Full'
             />
             <TripsContainer
             tripCode='PRC15-2019'
@@ -138,25 +78,25 @@ export const Container5 = ({ translate }) => (
             tripCode='PRC18-2019'
             dates='02-03 Feb 2019'
             boat='January Princess'
-            avalibility='10'
+            avalibility='Full'
             />
             <TripsContainer
             tripCode='PRC19-2019'
             dates='09-10 Feb 2019'
             boat='January Princess'
-            avalibility='20'
+            avalibility='Full'
             />
             <TripsContainer 
             tripCode='PRC20-2019'
             dates='16-17 Feb 2019'
             boat='January Princess'
-            avalibility='20'
+            avalibility='Full'
             />
             <TripsContainer 
             tripCode='PRC21-2019'
             dates='23-24 Feb 2019'
             boat='January Princess'
-            avalibility='Full'
+            avalibility='20 Slots'
             />
           </ScheduleContainer>
         </Fragment>
@@ -174,19 +114,19 @@ export const Container6   = ({ translate }) => (
             tripCode='PRC23-2019'
             dates='09-10 Mar 2019'
             boat='January Princess'
-            avalibility='20'
+            avalibility='Full'
             />
             <TripsContainer 
             tripCode='PRC24-2019'
             dates='16-17 Mar 2019'
             boat='January Princess'
-            avalibility='18'
+            avalibility='18 Slots'
             />
             <TripsContainer 
             tripCode='PRC25-2019'
             dates='23-24 Mar 2019'
             boat='January Princess'
-            avalibility='20'
+            avalibility='Full'
             />
             <TripsContainer 
             tripCode='PRC25-2019'
